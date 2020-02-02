5d8f092132ed3cf71c12a2accd264370295da92e
Daelamar <redfess@yandex.ru>
2020-02-02 18:51:13 +0300

    Поставил обработчики,меняющие цвет мантии и глаз, сделал валидацию формы. написал функции открытия и закрытия окна статистики

--- js/setup.js
+++ js/setup.js
@@ -1,4 +1,9 @@
 'use strict';
+var ESC_KEY = 27;
+var ENTER_KEY = 13;
+var MIN_NAME_LENGTH = 2;
+var MAX_NAME_LENGTH = 25;
+
 // Колличество похожих магов
 var SIMILAR_WIZARDS = 4;
 
@@ -13,6 +18,36 @@ var wizardListElement = document.querySelector('.setup-similar-list');
 
 // Находим шаблон для копирования магов
 var wizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+
+// Находим блок с фото пользователя для открытия окна статистики
+var setupOpenButton = document.querySelector('.setup-open-icon');
+
+// Находим кнопку для закрытия окна статистики
+var setupCloseButton = setup.querySelector('.setup-close');
+
+// Находим кнопку для закрытия окна статистики
+var userNameField = setup.querySelector('.setup-user-name');
+
+// Находим своего персонажа в окне статистики и нужные нам элементы (плащ, фаербол и т.д.)
+var userWizard = setup.querySelector('.setup-wizard');
+var userWizardEyes = userWizard.querySelector('.wizard-eyes');
+var userWizardCoat = userWizard.querySelector('.wizard-coat');
+var userWizardFireballWrap = setup.querySelector('.setup-fireball-wrap');
+var userWizardFirebal = setup.querySelector('.setup-fireball');
+
+// Находим нужные нам скрытые инпуты
+var userWizardEyesInput = setup.querySelector('[name="eyes-color"]');
+var userWizardCoatInput = setup.querySelector('[name="coat-color"]');
+var userWizardFireballInput = setup.querySelector('[name="fireball-color"]');
+
+// Создаем массив цветов для фаербола
+var fireballColors = [
+  '#ee4830',
+  '#30a8ee',
+  '#5ce6c0',
+  '#e848d5',
+  '#e6e848',
+];
 
 // Создаем массив имен
 var names = [
@@ -35,7 +70,7 @@ var secondNames = [
   'Онопко',
   'Топольницкая',
   'Нионго',
-  'Ирвинг'
+  'Ирвинг',
 ];
 
 // Создаем массив цветов для мантии
@@ -45,7 +80,7 @@ var mantleColors = [
   'rgb(146, 100, 161)',
   'rgb(56, 159, 117)',
   'rgb(215, 210, 55)',
-  'rgb(0, 0, 0)'
+  'rgb(0, 0, 0)',
 ];
 
 // Создаем массив цветов для глаз
@@ -54,7 +89,7 @@ var eyeColors = [
   'red',
   'blue',
   'yellow',
-  'green'
+  'green',
 ];
 
 // Функция случайного числа в диапазоне
@@ -67,15 +102,26 @@ var getRandomItem = function (array) {
   return array[getRandomNumber(0, array.length - 1)];
 };
 
+// Функция закрытия окна по нажатию ESC
+var onEscCloseSetupHandler = function (evt) {
+  if (evt.keyCode === ESC_KEY && userNameField !== evt.target) {
+    hideSetupWindowHandler();
+  }
+};
+
 // Функция открытия окна статистики
-var showSetupWindow = function () {
+var showSetupWindowHandler = function () {
   setup.classList.remove('hidden');
+  document.addEventListener('keydown', onEscCloseSetupHandler);
+  userNameField.addEventListener('input', userNameFieldValidityHandler);
 };
 
 // Функция закрытия окна статистики
-// var hideSetupWindow = function () {
-//   setup.classList.add('hidden');
-// };
+var hideSetupWindowHandler = function () {
+  setup.classList.add('hidden');
+  document.removeEventListener('keydown', onEscCloseSetupHandler);
+  userNameField.removeEventListener('input', userNameFieldValidityHandler);
+};
 
 // Функция открытия окна похожих магов
 var showSimilarWizardsBlock = function () {
@@ -94,7 +140,7 @@ var createWizards = function (number) {
     var wizard = {
       name: getRandomItem(names) + ' ' + getRandomItem(secondNames),
       coatColor: getRandomItem(mantleColors),
-      eyesColor: getRandomItem(eyeColors)
+      eyesColor: getRandomItem(eyeColors),
     };
     wizardsArr.push(wizard);
   }
@@ -121,7 +167,52 @@ var renderSimilarWizards = function (wizardObjects) {
   wizardListElement.appendChild(fragment);
 };
 
+// Функция валидности поля имени
+var userNameFieldValidityHandler = function (evt) {
+  var target = evt.target;
+  if (target.value.length < MIN_NAME_LENGTH) {
+    target.setCustomValidity('Имя персонажа должно содержать не меньше ' + MIN_NAME_LENGTH + '-х символов!');
+  } else if (target.value.length > MAX_NAME_LENGTH) {
+    target.setCustomValidity('Имя персонажа не должно содержать более ' + MAX_NAME_LENGTH + '-ти символов!');
+  } else if (target.value.length === 0) {
+    target.setCustomValidity('Обязательное поле!');
+  } else {
+    target.setCustomValidity('');
+  }
+};
+
+// Вешаем обработчики
+setupOpenButton.addEventListener('click', showSetupWindowHandler);
+setupOpenButton.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEY) {
+    showSetupWindowHandler();
+  }
+});
+
+setupCloseButton.addEventListener('click', hideSetupWindowHandler);
+setupCloseButton.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEY) {
+    hideSetupWindowHandler();
+  }
+});
+
+setup.addEventListener('click', function (evt) {
+  if (evt.target === userWizardFirebal) {
+    userWizardFireballWrap.style.background = getRandomItem(fireballColors);
+    userWizardFireballInput.value = userWizardFireballWrap.style.background;
+  }
+});
+
+userWizard.addEventListener('click', function (evt) {
+  if (evt.target === userWizardCoat) {
+    userWizardCoat.style.fill = getRandomItem(mantleColors);
+    userWizardCoatInput.value = userWizardCoat.style.fill;
+  } else if (evt.target === userWizardEyes) {
+    userWizardEyes.style.fill = getRandomItem(eyeColors);
+    userWizardEyesInput.value = userWizardEyes.style.fill;
+  }
+});
+
 // Вызываем функции
-showSetupWindow();
 renderSimilarWizards(createWizards(SIMILAR_WIZARDS));
 showSimilarWizardsBlock();
